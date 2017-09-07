e650bbc26bc130bd058f2646c82c525f6f626261
NICOLAS-PC\Nicolas <nicolaslen@gmail.com>
2017-09-07 13:03:59 -0300

    Error arreglado

--- 01-CodigoRepetido/C#/CatchExceptionsForTesting.cs
+++ 01-CodigoRepetido/C#/CatchExceptionsForTesting.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace IdiomExercise
 {
-    public class CatchExceptionsForTesting : ExceptionsOnTesting
+    public class CatchExceptionsForTesting
     {
         public void CatchAddingACustomerWithEmptyName(CustomerBook customerBook, Exception exceptionThrown)
         {
